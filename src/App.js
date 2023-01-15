753f77a4d3546d848fe4c471feb0a5a91d42038e
Elizybbethy <115698847+Elizybbethy@users.noreply.github.com>
2023-01-15 19:49:44 +0000

    State changes

--- src/App.js
+++ src/App.js
@@ -3,13 +3,22 @@ import * as React from "react";
 import Card from "./Card.js";
 
 
-const users = ({title: 'try',  content: 'learn'});
-export default 
+// const users = ({title: 'try',  content: 'learn'});
+export default function App() {
+  // const [users, account]= useState({username: "ElizyBbethy", accountname: "Twitter", Home: "Home", content:"welcome to the free world!",});
+  // function cardchanges() {
+  //   users.username
+  // }
+  // const handleChange = (e) => {
+  //   let updatedValue = {};
+  //   updatedValue = {username:e.target.value}
+  // }
 
-function App() {
+
+
   return (
     <div>
-      <Card username="ElizyBbethy" account="Twitter" Home="Home" content="welcome to the free world!"/>
+      <Card username="ElizyBbethy" accountname="Twitter" Home="Home" content="welcome to the free world!" comment="comment on this post"/>
     </div>
   );
 }
--- src/Card.js
+++ src/Card.js
@@ -1,14 +1,28 @@
 import { Grid, Paper, Typography } from '@mui/material';
 import React from 'react';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import {useState, useEffect} from 'react';
 
 
 
 
 export default function Card(props) {
+//     const [users, account]= useState({});
+// const usersSet = () => {
+    // const name = {
+    //     firstname: "try",
+    //     lastname: 'trial'
+    // }
+    // setUsers(prevState => name);
+    const [name, setName] = useState({firstName:"", latName:""})
+// }
+//   function cardchanges() {
+//     users.username
+//   }
     
     return (
      <div>
+        
         <Grid container spancing={2} justifyContent="center" style={{backgroundColor:"#ccb1f2"}}>
             <Grid item sm={8} md={6}>
                 <Paper variant='outlined' elevation={4} sx={{p: 2, margin:5, maxWidth: 400, backgroundColor: "#f2deb1"}}>
@@ -18,7 +32,7 @@ export default function Card(props) {
             <Grid item sm={8} md={6}>
                 <Typography gutterBottom variant='tittle' color='primary'>{props.Home}</Typography>
                 <Grid item>
-                <Typography gutterButton variant='subtittle2' color="primary">{props.account}</Typography>
+                <Typography gutterButton variant='subtittle2' color="primary">{props.accountname}</Typography>
             </Grid>
             </Grid>
         <Grid container alignItems="center">
@@ -29,6 +43,10 @@ export default function Card(props) {
         <Grid container>
             <Grid item sm={8} md={6}>
                 <Typography color="text.secondary" variant='body3'>{props.content}</Typography>
+            </Grid>
+            <Grid>
+                <Typography color="text.secondary" variant="body4">{props.comment}</Typography>
+                <input type="text" value={name.firstName} onChange={e => setName({firstName: e.target.value})}/>
             </Grid>
         </Grid>
         {/* </Card> */}
