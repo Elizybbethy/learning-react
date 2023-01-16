bbc3fe940406a8609efa8feaa4de2ca138195071
Musasizi Kenneth <kennymusasizi@hotmail.com>
2023-01-16 10:16:32 +0000

    Merge commit

--- src/App.js
+++ src/App.js
@@ -6,18 +6,6 @@ import Card from "./Card.js";
 // const users = ({title: 'try',  content: 'learn'});
 export default function App() {
   const [userDetails, setUserDetails] = React.useState({ username: "lizy", accountname: "twits", comment: 'like' })
-
-  // const [users, account]= useState({username: "ElizyBbethy", accountname: "Twitter", Home: "Home", content:"welcome to the free world!",});
-  // function cardchanges() {
-  //   users.username
-  // }
-  // const handleChange = (e) => {
-  //   let updatedValue = {};
-  //   updatedValue = {username:e.target.value}
-  // }
-
-
-
   return (
     <div>
       <Card userDetails={userDetails} />
--- src/Card.js
+++ src/Card.js
@@ -2,70 +2,58 @@ import { Grid, Paper, Typography } from '@mui/material';
 import React from 'react';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 
 
 
 
-export default function Card(props) {
-//     const [users, account]= useState({});
-// const usersSet = () => {
-    // const name = {
-    //     firstname: "try",
-    //     lastname: 'trial'
-    // }
-    // setUsers(prevState => name);
-    // const [users, accounts, comments] = useState({username:"lizy", accountname:"twits", comment:'like'})
-    const [users, account, comments] = useState({username:"lizy", accountname:"twits", comment:'like'})
-// }
-//   function cardchanges() {
-//     users.username
-//   }
-    
+export default function Card({ userDetails }) {
+
+
     return (
-     <div>
-        
-        <Grid container spancing={2} justifyContent="center" style={{backgroundColor:"#ccb1f2"}}>
-            <Grid item sm={8} md={6}>
-                <Paper variant='outlined' elevation={4} sx={{p: 2, margin:5, maxWidth: 400, backgroundColor: "#f2deb1"}}>
-        
-        {/* <Card variant="outlined"> */}
-        <Grid container>
-            <Grid item sm={8} md={6}>
-                <Typography gutterBottom variant='tittle' color='primary'>{props.Home}</Typography>
-                <Grid item>
-                <Typography gutterButton variant='subtittle2' color="primary">{props.accountName}</Typography>
+        <div>
+
+            <Grid container spancing={2} justifyContent="center" style={{ backgroundColor: "#ccb1f2" }}>
+                <Grid item sm={8} md={6}>
+                    <Paper variant='outlined' elevation={4} sx={{ p: 2, margin: 5, maxWidth: 400, backgroundColor: "#f2deb1" }}>
+
+                        {/* <Card variant="outlined"> */}
+                        <Grid container>
+                            <Grid item sm={8} md={6}>
+                                <Typography gutterBottom variant='tittle' color='primary'>{userDetails.Home}</Typography>
+                                <Grid item>
+                                    <Typography gutterButton variant='subtittle2' color="primary">{userDetails.accountname}</Typography>
+                                </Grid>
+                            </Grid>
+                            <Grid container alignItems="center">
+                                <AccountCircleIcon fontSize="large"></AccountCircleIcon>
+                                <Typography color="text.secondary" variant='body2'>{userDetails.username}</Typography>
+                            </Grid>
+                        </Grid>
+                        <Grid container>
+                            <Grid item sm={8} md={6}>
+                                <Typography color="text.secondary" variant='body3'>{userDetails.content}</Typography>
+                            </Grid>
+                            <Grid container>
+                                <Grid item sm={8} md={6}>
+                                    {/* <Typography color="text.secondary" variant="body4">{userDetails.comment}</Typography> */}
+                                    {/* <input type="text" value={name.firstName} onChange={e => setName({firstName: e.target.value})}/> */}
+                                    {/* <Typography value={users.username} onChange={e => setName({username: e.target.value})}> </Typography> */}
+                                    {/* <Typography value={hi.hello} onChange={e => setName({hello: e.target.value})}> </Typography> */}
+                                    <Typography gutterButton variant='subtittle2' color="primary">{userDetails.username}</Typography>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                        <Grid container>
+                            <Grid item>
+                                <FavoriteBorderIcon></FavoriteBorderIcon>
+                            </Grid>
+                        </Grid>
+                        {/* </Card> */}
+                    </Paper>
+                </Grid>
             </Grid>
-            </Grid>
-        <Grid container alignItems="center">
-        <AccountCircleIcon fontSize="large"></AccountCircleIcon>
-        <Typography color="text.secondary" variant='body2'>{props.username}</Typography>
-        </Grid>
-        </Grid>
-        <Grid container>
-            <Grid item sm={8} md={6}>
-                <Typography color="text.secondary" variant='body3'>{props.content}</Typography>
-            </Grid>
-            <Grid container>
-            <Grid item sm={8} md={6}>
-                {/* <Typography color="text.secondary" variant="body4">{props.comment}</Typography> */}
-                {/* <input type="text" value={name.firstName} onChange={e => setName({firstName: e.target.value})}/> */}
-                {/* <Typography value={users.username} onChange={e => setName({username: e.target.value})}> </Typography> */}
-                {/* <Typography value={hi.hello} onChange={e => setName({hello: e.target.value})}> </Typography> */}
-                <Typography gutterButton variant='subtittle2' color="primary">{users.username}</Typography>
-            </Grid>
-            </Grid>
-        </Grid>
-        <Grid container>
-            <Grid item>
-            <FavoriteBorderIcon></FavoriteBorderIcon>
-            </Grid>
-        </Grid>
-        {/* </Card> */}
-            </Paper>
-            </Grid>
-        </Grid>
-           
+
         </div>
     )
 }
